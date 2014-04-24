d271fbbb73bdcd14291c6e92bd2e6ba37975d2ef
ekranac <besal.ziga@gmail.com>
2014-04-24 12:05:20 +0200

    app

---
+++ MyApp/src/main/java/com/ziga/myapp/app/DisplayMessageActivity.java
@@ -0,0 +1,42 @@
+package com.ziga.myapp.app;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by ziga on 23.4.2014.
+ */
+public class DisplayMessageActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setTextColor(Color.parseColor("#73C7E8"));
+        textView.setText(message);
+
+        setContentView(textView);
+    }
+
+    /*@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }*/
+}
